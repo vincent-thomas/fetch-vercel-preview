f3b5220e6849e68d8a726dba2111418638587298
Vincent Thomas <vincent.nathan.thomas@gmail.com>
2022-06-27 16:06:58 +0200

    chore: removed node_modules

---
+++ index.ts
@@ -0,0 +1,27 @@
+// @ts-check
+
+import * as core from "@actions/core";
+import axios from "axios";
+
+async function main() {
+  try {
+    const vercelToken = core.getInput("vercel_token");
+    const projectId = core.getInput("vercel_project_id");
+
+    const { data: json } = await axios.get(
+      `https://api.vercel.com/v6/deployments?projectId=${projectId}&state=READY&limit=1`,
+      {
+        headers: {
+          Authorization: `Bearer ${vercelToken}`,
+        },
+      }
+    );
+
+    const url = json.deployments[0].url;
+    core.setOutput("preview_url", url);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
