3c80df3e6304330a7167b0fea145dc76587bb265
Vincent Thomas <vincent.nathan.thomas@gmail.com>
2022-06-27 15:49:55 +0200

    perf: using axios instead of fetch

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+// @ts-check
 const github = require("@actions/github");
 const core = require("@actions/core");
+const { default: axios } = require("axios");
 
 async function main() {
   try {
-    const context = github.context;
     const vercelToken = core.getInput("vercel_token");
     const projectId = core.getInput("vercel_project_id");
 
-    const response = await fetch(
+    const { data: json } = await axios.get(
       `https://api.vercel.com/v6/deployments?projectId=${projectId}&state=READY&limit=1`,
       {
         headers: {
           Authorization: `Bearer ${vercelToken}`,
         },
-        method: "GET",
       }
     );
-    const json = await response.json();
+
     const url = json.deployments[0].url;
     core.setOutput("preview_url", url);
   } catch (error) {
